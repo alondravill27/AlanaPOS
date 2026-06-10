905e1248c925afe004d10baf5120cfa7bb2b2902
ALONDRA ANAID VILLEGAS ITURRIOS <22030627@uadeo.mx>
2026-06-10 13:22:33 -0700

    CAMBIOSSSSSS

---
+++ AlanaPOS/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace AlanaPOS.WinUI; // Volvemos al namespace correcto de la plataforma
+
+/// <summary>
+/// Proporciona un comportamiento específico de la aplicación para complementar la clase Application predeterminada.
+/// </summary>
+public partial class App : MauiWinUIApplication
+{
+    /// <summary>
+    /// Inicializa el objeto de aplicación de destino.
+    /// </summary>
+    public App()
+    {
+        // ELIMINAMOS this.InitializeComponent() de aquí para evitar el error CS0111
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
